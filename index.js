d2ded931ef00234b4eeee254572518a280fb2639
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-05 02:57:23 +0300

    commit tree 6638d83332b4e2cdee9ed90b3a96e5afb61a2e5d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const names = [];
+const messages =[];
+
+
+function writeCards(names, event ) {
+    for (let i=0; i < names.length; i++){
+      
+        const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+        messages.push(message);
+       
+        
+    }
+    return messages;
+}
+writeCards();
+
+
+function countDown(n){
+    let count = 0;
+    while (count <= n){
+        console.log(count);
+        count++;
+      
+    }
+ 
+}
+countDown(10);
